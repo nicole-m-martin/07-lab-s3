2995bc677f38ac5a137af1fe0bea7673871a860e
Nicole Martin <n.martinpdx@gmail.com>
2021-03-31 17:54:23 -0700

    add file tree and readme

---
+++ __tests__/app.test.js
@@ -0,0 +1,12 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('07-lab routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('');
+});
